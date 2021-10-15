e138137d7b39e80275ba1012f686f8dcea69be58
RafaelClaumann <rafael_claumann@hotmail.com>
2021-10-15 20:44:21 -0300

    arquivos de infra
    comando: vagrant up servidor, na pasta infra para subir a maquina, gerar as chaves dentro da pasta keys antes

---
+++ infra/keys/readme.txt
@@ -0,0 +1,4 @@
+# Garantindo as chaves
+# Este comando gera dois arquivos (key e key.pub)
+# Necessario para se conectar via SSH no servidor
+#  ssh-keygen -q -t rsa -f key -N ''
---
+++ infra/provision.sh
@@ -0,0 +1,30 @@
+# Garantindo as chaves
+#  ssh-keygen -q -t rsa -f key -N ''
+
+KEY_PATH='/vagrant/keys'
+mkdir -p /root/.ssh
+cp $KEY_PATH/key /root/.ssh/id_rsa
+cp $KEY_PATH/key.pub /root/.ssh/id_rsa.pub
+cp $KEY_PATH/key.pub /root/.ssh/authorized_keys
+chmod 400 /root/.ssh/id_rsa*
+cat /root/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+
+# Garantindo os hosts
+HOSTS=$(head -n7 /etc/hosts)
+echo -e "$HOSTS" > /etc/hosts
+echo '10.20.20.100 server.machine' >> /etc/hosts
+
+# https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-on-ubuntu-20-04-pt
+apt-get update
+apt-get install default-jdk -y
+
+# https://www.digitalocean.com/community/tutorials/how-to-install-jenkins-on-ubuntu-18-04-pt
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+apt-add-repository "deb https://pkg.jenkins.io/debian-stable binary/"
+apt-get install jenkins -y
+
+# instalando docker via script de conveniência
+curl -fsSL https://get.docker.com | bash
+systemctl start docker
+systemctl enable docker
+usermod -aG docker vagrant
